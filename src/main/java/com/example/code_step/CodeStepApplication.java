6fb1df25985f7ba8647cbbc2ba444f3c12328413
soyoon19 <96063941+soyoon19@users.noreply.github.com>
2024-07-23 00:43:26 +0900

    start

---
+++ src/main/java/com/example/code_step/CodeStepApplication.java
@@ -0,0 +1,13 @@
+package com.example.code_step;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeStepApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodeStepApplication.class, args);
+    }
+
+}
