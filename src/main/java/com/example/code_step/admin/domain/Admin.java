4141c9e1d6f3d1cc6f7822607ebebcf60668e249
soyoon19 <96063941+soyoon19@users.noreply.github.com>
2024-08-12 13:55:08 +0900

    feat : admin, quest 도메인 admin 도메인 수정, quest 도메인 생성

--- src/main/java/com/example/code_step/admin/domain/Admin.java
+++ src/main/java/com/example/code_step/admin/domain/Admin.java
@@ -1,5 +1,34 @@
 package com.example.code_step.admin.domain;
 
+import com.example.code_step.member.domain.Member;
+import com.example.code_step.problem.domain.Problem;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Admin {
+    List<Member> member = new ArrayList<>();
+    List<Problem> problem = new ArrayList<>();
+    //admin
+    public void joinAdmin(){}
+    public void loginAdmin(){}
+    //problem
     public void makeProblem(){}
+    public void updateProblem(){}
+    public void deleteProblem(){}
+    public List<Problem> viewProblem(){return problem;}
+    //course
+    public void makeCourse(){}
+    public void updateCourse(){}
+    public void deleteCourser(){}
+    //item
+    public void makeItem(){}
+    public void updateItem(){}
+    public void deleteItem(){}
+    //quest
+    public void makeQuest(){}
+    public void updateQuest(){}
+    public void deleteQuest(){}
+    //member
+    public List<Member> viewMember(){return member;}
 }
---
+++ src/main/java/com/example/code_step/quest/domain/Quest.java
@@ -0,0 +1,7 @@
+package com.example.code_step.quest.domain;
+
+public class Quest {
+    public void passJudgment(){}
+    public void giveCoin(){}
+    public void giveExp(){}
+}
